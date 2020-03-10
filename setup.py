0382c588b49d5c468d15c2871d5479939521d01f
Vitor <vitormarquespio@gmail.com>
2020-03-10 20:37:04 -0300

    Version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('LICENSE') as f:
     LICENSE = f.read()
     setup(
         name="pybible-cli",
-        version="1.1.2",
+        version="1.1.3",
         description="Bible reference CLI application",
         long_description_content_type="text/markdown",
         long_description=README,
