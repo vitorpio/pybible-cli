0d00ee2fc02f05570d45e720dfc6975ca8409277
Vitor <vitormarquespio@gmail.com>
2020-03-25 18:34:53 -0300

    Python logic to read specific json file of the correct book

--- pybible/pybible_load.py
+++ pybible/pybible_load.py
@@ -1,7 +1,8 @@
 import jsonpickle
 import sys
+import importlib
 from pybible.classes.bible import Bible
-import pybible.resources.bibles_serialized
+
 try:
     from importlib.resources import open_text
 except ImportError:
@@ -10,12 +11,14 @@ except ImportError:
 JSON_EXT = ".json"
 
 
-def load(bible_name: str = "kj") -> Bible:
+def load(bible_name: str = "kj", json_name: str = "kj") -> Bible:
     try:
-        json_file = open_text(pybible.resources.bibles_serialized, bible_name
+        bible_package = importlib.import_module(
+            f'pybible.resources.bibles_serialized.{bible_name}')
+        json_file = open_text(bible_package, json_name
                               + JSON_EXT)
         return jsonpickle.decode(json_file.read())
-    except OSError:
-        print(f"Could not load \"{bible_name}\" json file, check if it exists "
+    except ImportError:
+        print(f"Could not load \"{json_name}\" json file, check if it exists "
               f"and is structure correctly as described in the docs.")
         sys.exit(1)
